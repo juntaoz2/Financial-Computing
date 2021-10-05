94733a39a31d5cb83ee872569c34e741d876bfe3
juntaoz2 <44959696+juntaoz2@users.noreply.github.com>
2021-10-05 11:44:24 -0400

    Add files via upload

---
+++ hw4.cpp
@@ -0,0 +1,59 @@
+//
+//  hw4.cpp
+//  Homework4
+//
+//  Created by 张君陶 on 2021/10/5.
+//
+
+#include "hw4.hpp"
+//#pragma once
+#include <iostream>
+#include <cmath>
+#include "BinomialTreeModel02.hpp"
+
+using namespace std;
+
+//namespace fre {
+EurOption::~EurOption() {}
+
+double Call::Payoff(double z) const {
+    if(z>K) {
+        return z-K;}
+    return 0.0;
+}
+
+double Put::Payoff(double z) const {
+    if(z<K) {
+        return K-z;}
+    return 0.0;
+}
+
+double BullSpread::Payoff(double z) const {
+    if (z>=K2) {
+        return K2-K1;}
+    if (z<=K1) {
+        return 0.0;}
+    return z-K1;}
+
+double BearSpread::Payoff(double z) const {
+    if (z>=K2) {
+        return 0.0;}
+    if (z<=K1) {
+        return K2-K1;}
+    return K2-z;}
+
+
+double OptionCalculation::PriceByCRR(const BinomialTreeModel& Model) {
+    double optionPrice = 0.0;
+    double q = Model.RiskNeutProb();
+    int N = pOption->GetN();
+    double* pPrice = new double[N + 1];
+    for (int i = 0; i <= N; i++) {
+        pPrice[i] = pOption->Payoff(Model.CalculateAssetPrice(N, i)); }
+    for (int n = N - 1; n >= 0; n--) {
+        for (int i = 0; i <= n; i++){
+            pPrice[i] = (q * pPrice[i + 1] + (1 - q) * pPrice[i]) / Model.GetR();}}
+    optionPrice = pPrice[0];
+    delete[] pPrice;
+    pPrice = nullptr;
+    return optionPrice;}
---
+++ main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  Homework4
+//
+//  Created by 张君陶 on 2021/10/4.
+//
+
+#include <iostream>
+#include <iomanip>
+#include "BinomialTreeModel02.hpp"
+#include "hw4.hpp"
+
+using namespace std;
+
+
+int main(){
+    int N=8;
+    double U = 1.15125, D = 0.86862, R = 1.00545;
+    double S0 = 106.00, K = 100.00;
+    double K1 = 100, K2=110;
+    
+    BinomialTreeModel Model(S0, U, D, R);
+    
+    Call call(N, K);
+    OptionCalculation callCalculation(&call);
+    cout << "European call = " << fixed << setprecision(2) << callCalculation.PriceByCRR(Model) << endl;
+    
+    Put put(N, K);
+    OptionCalculation putCalculation(&put);
+    cout << "European put = "<< fixed << setprecision(2) << putCalculation.PriceByCRR(Model) << endl;
+    
+    BullSpread BullS(N, K1, K2);
+    OptionCalculation bullspreadCalculation(&BullS);
+    cout << "Bull Spread = " << fixed << setprecision(2) << bullspreadCalculation.PriceByCRR(Model) << endl;
+    
+    BearSpread BearS(N, K1, K2);
+    OptionCalculation bearspreadCalculation(&BearS);
+    cout << "Bear Spread = "<< fixed << setprecision(2) << bearspreadCalculation.PriceByCRR(Model) << endl; return 0;
+}
